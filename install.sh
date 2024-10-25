b962e6a02ef3d5214db65fa8960e228e8163b8c4
diablopizza <alexanderbezzhon@gmail.com>
2024-10-25 19:28:15 +0500

    Update install.sh

--- install.sh
+++ install.sh
@@ -8,18 +8,14 @@ makepkg -si
 cd
 sudo pacman -Syu
 
-clear
-
-sudo pacman -S git hyprland hyprpicker waybar swayimg swaync swappy swaylock swww nemo fish micro vlc yad
+sudo pacman -S git hyprland hyprpicker waybar swayimg swaync swappy swaylock swww nemo fish micro vlc yad rofi
 
 # FONT
 sudo pacman -S ttf-jetbrains-mono-nerd
-clear
 
 # GTK
 sudo pacman -S nwg-look
 yay -S dracula-gtk-theme dracula-icons-theme bibata-cursor-theme
-clear
 
 gsettings set org.gnome.desktop.interface gtk-theme "Dracula"
 gsettings set org.gnome.desktop.interface icon-theme "Dracula"
@@ -27,19 +23,16 @@ gsettings set org.gnome.desktop.interface gtk-theme "Bibata-Modern-Ice"
 
 # UTILITIES
 sudo pacman -S htop ranger mc sl glava dosfstools networkmanager neofetch fastfetch python python-pip lsd tmux bat
-clear
 sudo yay -S cava pipes.sh
-clear
+
 # PROGRAMS
 sudo pacman -S firefox obs-studio obsidian
-clear
 
 git clone https://github.com/kiskis1488/Hyprland-Dots.git
 cd Hyprland-Dots
 cp -r {hypr,waybar,rofi,kitty,cava,fish,scripts} ~/.config/
 cd
 rm -rf Hyprland-Dots
-clear
 
 git clone https://github.com/michaelScopic/Wallpapers.git
 mkdir Pictures
