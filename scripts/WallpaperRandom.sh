e1a898bb06f28555fb84ac3eccacc2006629de21
diablopizza <alexanderbezzhon@gmail.com>
2024-10-25 18:39:36 +0500

    Add files via upload

---
+++ scripts/WallpaperRandom.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+wallDIR="$HOME/Pictures/Wallpapers/random"
+
+focused_monitor=$(hyprctl monitors | awk '/^Monitor/{name=$2} /focused: yes/{print name}')
+
+PICS=($(find ${wallDIR} -type f \( -name "*.jpg" -o -name "*.jpeg" -o -name "*.png" -o -name "*.gif" \)))
+RANDOMPICS=${PICS[ $RANDOM % ${#PICS[@]} ]}
+
+
+# Transition config
+FPS=60
+TYPE="center"
+DURATION=1
+BEZIER=".43,1.19,1,.4"
+SWWW_PARAMS="--transition-fps $FPS --transition-type $TYPE --transition-duration $DURATION --transition-bezier $BEZIER"
+
+
+swww query || swww-daemon --format xrgb && swww img -o $focused_monitor ${RANDOMPICS} ${SWWW_PARAMS}
