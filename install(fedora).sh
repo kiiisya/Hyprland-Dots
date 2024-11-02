70085646003d2e93d7ea6bced238ae153c5ffad0
kiskis <alexanderbezzhon@gmail.com>
2024-11-02 19:52:35 +0500

    Update install(fedora).sh

--- install(fedora).sh
+++ install(fedora).sh
@@ -6,6 +6,12 @@ sudo dnf install git hyprland hyprpicker waybar swappy swaylock nemo fish micro 
 
 sudo dnf install htop ranger mc sl glava dosfstools neofetch fastfetch python python-pip lsd tmux bat
 
+git clone https://github.com/kiskis1488/Hyprland-Dots.git
+cd Hyprland-Dots
+cp -r {hypr,waybar,rofi,kitty,cava,fish,scripts} ~/.config/
+cd
+rm -rf Hyprland-Dots
+
 git clone https://github.com/michaelScopic/Wallpapers.git
 mkdir Pictures
 cp -r Wallpapers ~/Pictures/
