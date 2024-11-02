358110c40b37190f7d2716f07626d56ef409b186
kiskis <alexanderbezzhon@gmail.com>
2024-11-02 19:58:17 +0500

    Update install(fedora).sh

--- install(fedora).sh
+++ install(fedora).sh
@@ -4,7 +4,16 @@ sudo dnf update
 
 sudo dnf install git hyprland hyprpicker waybar swappy swaylock nemo fish micro vlc yad rofi
 
-sudo dnf install htop ranger mc sl glava dosfstools neofetch fastfetch python python-pip lsd tmux bat
+sudo dnf install htop ranger mc sl glava dosfstools neofetch fastfetch python python-pip lsd tmux bat cava
+
+sudo dnf install obs-studio
+
+wget https://download.jetbrains.com/fonts/JetBrainsMono-2.304.zip
+unzip JetBrainsMono-2.304.zip
+rm {AUTHORS.txt,OFL.txt,JetBrainsMono-2.304.zip}
+sudo mv fonts/ttf/* /usr/share/fonts
+cd
+rm -rf fonts
 
 git clone https://github.com/kiskis1488/Hyprland-Dots.git
 cd Hyprland-Dots
