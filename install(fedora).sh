fd79cd04b0174f4d2d20f9d369a89441447ce510
kiskis <alexanderbezzhon@gmail.com>
2024-11-02 19:49:56 +0500

    Update install(fedora).sh

--- install(fedora).sh
+++ install(fedora).sh
@@ -1,1 +1,7 @@
 #!/bin/bash
+
+sudo dnf update
+
+sudo dnf install git hyprland hyprpicker waybar swappy swaylock nemo fish micro vlc yad rofi
+
+sudo dnf install htop ranger mc sl glava dosfstools neofetch fastfetch python python-pip lsd tmux bat
