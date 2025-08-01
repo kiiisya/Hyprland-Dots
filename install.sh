82eaca888fcf83a98655595c9f1f10fccd0a94f3
kiiisya <devfox777@gmail.com>
2025-08-01 18:30:47 +0300

    Update install.sh

--- install.sh
+++ install.sh
@@ -3,49 +3,27 @@
 # UPDATE
 sudo pacman -Syu
 
+# PACMAN
+sudo pacman -S hyprland hyprpicker waybar swayimg swaync swappy swaylock swww nemo fish yad rofi # WM
+sudo pacman -S htop btop ranger mc sl glava dosfstools networkmanager neofetch fastfetch python python-pip lsd tmux git bat micro nwg-look # UTILITES
+sudo pacman -S firefox obs-studio obsidian vlc # PROGRAMS
+
 # YAY
-sudo pacman -S git
-git clone https://aur.archlinux.org/yay
-cd yay
-makepkg -si
-cd
-sudo pacman -Syu
-
-# WM
-sudo pacman -S hyprland hyprpicker waybar swayimg swaync swappy swaylock swww nemo fish micro yad rofi
-
-# FONT
-sudo pacman -S ttf-jetbrains-mono-nerd
-
-# UTILITIES
-sudo pacman -S htop ranger mc sl glava dosfstools networkmanager neofetch fastfetch python python-pip lsd tmux bat
-sudo yay -S cava pipes.sh
-
-# PROGRAMS
-sudo pacman -S firefox obs-studio obsidian vlc
+git clone https://aur.archlinux.org/yay & cd yay & makepkg -si & cd & sudo pacman -Syu
+yay -S cava pipes.sh
 
 # DOTS
-git clone https://github.com/kiskis1488/Hyprland-Dots.git
+git clone https://github.com/kiiisya/Hyprland-Dots.git
 cd Hyprland-Dots
-cp -r {hypr,waybar,rofi,kitty,cava,fish,scripts} ~/.config/
-cd
-rm -rf Hyprland-Dots
+mv -r {hypr,waybar,rofi,kitty,cava,fish,scripts} ~/.config/
 
 # GTK
-sudo pacman -S nwg-look
 yay -S dracula-gtk-theme dracula-icons-theme bibata-cursor-theme
-
-gsettings set org.gnome.desktop.interface gtk-theme "Dracula"
 gsettings set org.gnome.desktop.interface icon-theme "Dracula"
 gsettings set org.gnome.desktop.interface gtk-theme "Bibata-Modern-Ice"
 
 # WALLPAPERS
-git clone https://github.com/michaelScopic/Wallpapers.git
-mkdir Pictures
-cp -r Wallpapers ~/Pictures/
-cd Pictures/Wallpapers
-mkdir random
-cp -r ~/Hyprland-Dots/wallpapers ~/Pictures/Wallpapers/random
-clear
-echo "REBOOT"
+mkdir ~/.config/.wallpapers
+mv -r ~/Hyprand-Dots/wallpapers/* ~/.config/.wallpapers
+rm -rf Hyprland-Dots
 reboot
